69b935a812bfecbad4fa361980f17664e36cb834
luksquitos <lucasmartinscaet@hotmail.com>
2026-07-20 15:50:11 -0300

    feat: configuring phosphor icons and used icons into project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import { phosphorStarFill } from '@ng-icons/phosphor-icons/fill';
+import {
+  phosphorArrowLeft,
+  phosphorCalendar,
+  phosphorEnvelope,
+  phosphorFilmSlate,
+  phosphorMagnifyingGlass,
+  phosphorPassword,
+  phosphorPencilSimple,
+  phosphorPlay,
+  phosphorPlus,
+  phosphorPopcorn,
+  phosphorSignOut,
+  phosphorStar,
+  phosphorTag,
+  phosphorUploadSimple,
+  phosphorUser,
+  phosphorWarning,
+  phosphorX,
+  phosphorXCircle,
+} from '@ng-icons/phosphor-icons/regular';
 
 import { ItemCard } from './components/ui/item-card/item-card';
 
@@ -8,6 +30,29 @@ import { ItemCard } from './components/ui/item-card/item-card';
   imports: [RouterOutlet, ItemCard],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  providers: [
+    provideIcons({
+      phosphorPlay,
+      phosphorXCircle,
+      phosphorStar,
+      phosphorStarFill,
+      phosphorPopcorn,
+      phosphorFilmSlate,
+      phosphorMagnifyingGlass,
+      phosphorArrowLeft,
+      phosphorPlus,
+      phosphorEnvelope,
+      phosphorPassword,
+      phosphorSignOut,
+      phosphorX,
+      phosphorTag,
+      phosphorCalendar,
+      phosphorUploadSimple,
+      phosphorWarning,
+      phosphorUser,
+      phosphorPencilSimple,
+    }),
+  ],
 })
 export class App {
   protected readonly title = signal('gerenciador-de-filmes');
